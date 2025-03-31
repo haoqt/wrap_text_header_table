809b87d5074916b2364528399206862bd33f768b
Quách Thế Hào <115454587+haoqt@users.noreply.github.com>
2025-03-31 09:40:51 +0700

    [ADD] wrap_text_header_table

---
+++ wrap_text_header_table/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Wrap Text Header Table",
+
+    'summary': "Wrap Text Header Table",
+
+    'description': """
+        Wrap Text Header Table
+    """,
+    'author': "Visnu",
+    'category': 'category',
+    'version': '17.0.1.0.0',
+    'images': ['static/description/icon.png'],
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            '/wrap_text_header_table/static/src/scss/style.scss',
+        ]
+    },
+    "license": "LGPL-3",
+}
